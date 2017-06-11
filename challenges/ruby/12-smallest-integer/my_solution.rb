f5021522dc33ca15e61b8349fab7059a40687a66
oscar diaz <you@example.com>
2017-06-11 13:09:50 -0500

    reto 12 ruby ok :)

--- challenges/ruby/12-smallest-integer/my_solution.rb
+++ challenges/ruby/12-smallest-integer/my_solution.rb
@@ -5,4 +5,25 @@
 # Tu solucion abajo:
 
 
+def smallest_integer(array)
+	array.min
+end
 
+puts smallest_integer([1, 2, 3])       # => 1
+puts smallest_integer([0, -10, 10])    # => -10
+puts smallest_integer([-10, -20, -30]) # => -30
+
+
+# def smallest_integer(array)
+# 	menor = array[0]
+# 	array.each do |num|
+# 		if num < menor
+# 			menor = num
+# 		end
+# 	end
+# 	return menor
+# end
+
+# puts smallest_integer([1, 2, 3])       # => 1
+# puts smallest_integer([0, -10, 10])    # => -10
+# puts smallest_integer([-10, -20, -30]) # => -30
