8559ee3ebddee81a3e9f225c2bc646f01e648dfb
oscar diaz <you@example.com>
2017-06-11 15:33:47 -0500

    reto 15 ruby ok :)

--- challenges/ruby/15-longest-string/my_solution.rb
+++ challenges/ruby/15-longest-string/my_solution.rb
@@ -2,3 +2,10 @@
 
 # Tu solucion abajo:
 
+def longest_string(array)
+	array.max_by{ |elem| elem.length }  # => "three"
+end
+
+p longest_string(['cat', 'zzzzzzz', 'apples'])  # => "zzzzzzz"
+p longest_string(['']) # => ''
+p longest_string([]) # => nil
