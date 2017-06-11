b456929464e237c384a8735b2c216c3a860d2a3c
oscar diaz <you@example.com>
2017-06-11 13:19:45 -0500

    reto 13 ruby ok :)

--- challenges/ruby/13-largest-integer/my_solution.rb
+++ challenges/ruby/13-largest-integer/my_solution.rb
@@ -1,2 +1,22 @@
 # Largest Integer
 
+def largest_integer(array)
+	array.max
+end
+
+puts largest_integer([-10, 0, 10])    # => 10
+puts largest_integer([-10, -20, -30]) # => -10
+
+# def largest_integer(array)
+# 	mayor = array[0]
+# 	array.each do |num|
+# 		if num > mayor
+# 			mayor = num
+# 		end
+# 	end
+# 	return mayor
+# end
+
+# puts largest_integer([1, 2, 3])       # => 1
+# puts largest_integer([0, -10, 10])    # => -10
+# puts largest_integer([-10, -20, -30]) # => -30
