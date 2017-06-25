e646da34280786bb9b483920fd47ea7374a4e658
oscar diaz <you@example.com>
2017-06-25 15:47:17 -0500

    reto active record 2 ok :)

---
+++ challenges/active-record/2-creating-tables/source/db/migrate/20170621033000_create_peoples.rb
@@ -0,0 +1,12 @@
+class CreatePeoples < ActiveRecord::Migration
+	def change
+    create_table :peoples do |t|
+      t.string   :first_name
+      t.string   :last_name
+    
+      t.timestamps
+    end
+  end
+	
+	
+end
